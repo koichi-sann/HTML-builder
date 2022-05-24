af78ecc2d58e66a86b478949b0188599307c9827
koichi-sann <vasilyev62@gmail.com>
2022-05-24 04:32:48 +0300

    feat: add 04-copy-directory solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const path = require('path')
+const fsProm = require('fs/promises')
 
+const folderPath = path.join(__dirname, 'files')
+const folderPathCopy = path.join(__dirname, 'files-copy')
+
+const isFolderExist = async (fold) => {
+  const res = await fsProm
+    .access(fold)
+    .then(() => true)
+    .catch(() => false)
+  return res
+}
+
+const createCopy = async (fold) => {
+  if (await isFolderExist(folderPathCopy)) {
+    await fsProm.rm(folderPathCopy, { recursive: true })
+  }
+  await fsProm.mkdir(folderPathCopy)
+  fold.forEach(async (f) => {
+    await fsProm
+      .copyFile(path.join(folderPath, f), path.join(folderPathCopy, f))
+      .then(console.log(f))
+  })
+  return fold.length
+}
+
+fsProm
+  .readdir(folderPath)
+  .then((fold) => createCopy(fold))
+  .then((count) => console.log(`${count} file(s) copied!`))
