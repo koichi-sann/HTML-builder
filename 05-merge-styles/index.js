14595d84540c2b30612dd7300d044ff35298a5cf
koichi-sann <vasilyev62@gmail.com>
2022-05-26 16:32:50 +0300

    feat: 05-merge-styles solution

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,7 +1,24 @@
-process.stdout.write(
-  'Проверьте далее ближе к четвергу или напишите мне в дискорд: 幸一 (Kōichi)#7522\n'
-)
-process.on('SIGINT', () => {
-  console.log('Большое спасибо :)')
-  process.exit()
+const path = require('path')
+const fs = require('fs')
+
+const folderStyles = path.join(__dirname, 'styles')
+const folderBundle = path.join(__dirname, 'project-dist', 'bundle.css')
+
+const stream = fs.createWriteStream(folderBundle)
+
+fs.readdir(folderStyles, { withFileTypes: true }, (e, fls) => {
+  if (e) return `${e}`
+
+  fls.forEach((f) => {
+    if (f.isFile() && path.extname(f.name) === '.css') {
+      let style = ''
+
+      const pathS = path.join(folderStyles, f.name)
+      const rStream = fs.createReadStream(pathS, 'utf8')
+
+      rStream.on('data', (data) => (style += data))
+      rStream.on('end', () => stream.write(`${style}\n`))
+      rStream.on('error', (e) => console.log(e))
+    }
+  })
 })
